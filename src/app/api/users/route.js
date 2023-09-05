3cdcd9d73d3a1c93faaea442b35c09b63204301c
Jignesh <jignesh.sharma.707@gmail.com>
2023-09-05 23:07:35 +0530

    post request

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -5,3 +5,21 @@ export function GET() {
   const data = user;
   return NextResponse.json({ data }, { status: 200 });
 }
+
+export async function POST(request) {
+  let payload = await request.json();
+  console.log(payload.name);
+  console.log(payload.age);
+  console.log(payload.email);
+
+  if (!payload.name || !payload.age || !payload.email) {
+    return NextResponse.json(
+      { result: 'require field not found', success: false },
+      { status: 400 }
+    );
+  }
+  return NextResponse.json(
+    { result: 'New User Created', success: true },
+    { status: 201 }
+  );
+}
